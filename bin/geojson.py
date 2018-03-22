4a86f4c004ae9d37e0b08e9cc8524826ad448cc8
David Jones <drj@pobox.com>
2018-03-22 20:37:58 +0000

    Add home ward to data

--- bin/geojson.py
+++ bin/geojson.py
@@ -6,21 +6,40 @@ import sys
 def main():
     geojson = []
 
+    wards = {}
+
     with open("data/joined") as f:
         for row in f:
             cells = row.strip().split('\t')
-            geojson.append(dict(
-              type="LineString",
-              coordinates=[[cells[9], cells[8]], [cells[11], cells[10]]]
-            ))
-            geojson.append(dict(
+            home_ward = cells[12]
+            electoral_ward = cells[1]
+            electoral_point = [cells[11], cells[10]]
+            home_point = [cells[9], cells[8]]
+            if home_ward != electoral_ward:
+                geojson.append(dict(
+                  type="LineString",
+                  coordinates=[home_point, electoral_point]
+                ))
+            else:
+                geojson.append(dict(
+                  type="Feature",
+                  properties=dict(type="home"),
+                  geometry=dict(
+                    type="Point",
+                    coordinates=home_point
+                  )
+                ))
+            wards[electoral_ward] = dict(
               type="Feature",
-              properties=dict(name=cells[0]),
+              properties=dict(type="ward", name=electoral_ward),
               geometry=dict(
                 type="Point",
-                coordinates=[cells[11], cells[10]]
+                coordinates=electoral_point
               )
-            ))
+            )
+
+    for ward, feature in wards.items():
+        geojson.append(feature)
 
     with open("data/geom.geojson", 'w') as out:
         json.dump(geojson, out, indent=2)
