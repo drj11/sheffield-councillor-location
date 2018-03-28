d5f44669baccf30e6b24a950267b3636a17da207
David Jones <drj@pobox.com>
2018-03-28 21:34:42 +0100

    Add distance to ward

--- bin/geojson.py
+++ bin/geojson.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import math
 import sys
 
 
@@ -15,9 +16,10 @@ def main():
             home_ward = cells[13]
             electoral_ward = cells[1]
             i = 9
-            electoral_point = [cells[i+3], cells[i+2]]
-            home_point = [cells[i+1], cells[i]]
-            if home_ward != electoral_ward:
+            electoral_point = [float(cells[i+3]), float(cells[i+2])]
+            home_point = [float(cells[i+1]), float(cells[i])]
+
+            if home_ward != electoral_ward and False:
                 geojson.append(dict(
                   type="LineString",
                   coordinates=[home_point, electoral_point]
@@ -25,9 +27,17 @@ def main():
 
             colour = team_colour(cells[2])
 
+            properties = dict(type="home",
+                colour=colour,
+                representing=electoral_ward)
+
+            if home_ward != electoral_ward:
+                properties['distance'] = round(
+                  distance(home_point, electoral_point), 1)
+
             geojson.append(dict(
               type="Feature",
-              properties=dict(type="home", colour=colour),
+              properties=properties,
               geometry=dict(
                 type="Point",
                 coordinates=home_point
@@ -59,6 +69,38 @@ def team_colour(party):
         return '#b09'
     if "Liberal" in party:
         return '#fb3'
+
+
+def distance(p, q):
+    """Distance between p and q, where p and q
+    are on Earth's surface, given in (long, lat).
+    """
+
+    # Earth radius
+    R = 6371
+
+    def sin(d):
+        return math.sin(math.radians(d))
+
+    def cos(d):
+        return math.cos(math.radians(d))
+
+    def xyz(lon, lat):
+        x = cos(lat)*cos(lon)
+        y = cos(lat)*sin(lon)
+        z = sin(lat)
+        return x, y, z
+
+    px, py, pz = xyz(*p)
+    qx, qy, qz = xyz(*q)
+
+    dx = qx - px
+    dy = qy - py
+    dz = qz - pz
+    # chord length
+    C = (dx**2 + dy**2 + dz**2)**0.5
+    C *= R
+    return C
 
 
 if __name__ == '__main__':
